eaabb58f7a153b671449a3f56a84f1ca8fd8f4b6
Aziz Bouchtaoui <m.bouchaouiaziz@gmail.com>
2024-09-05 04:08:13 +0100

    Init

---
+++ src/main/java/com/sunlingua/sunlinguabackend/SunlinguaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sunlingua.sunlinguabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunlinguaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunlinguaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunlingua/sunlinguabackend/SunlinguaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunlingua.sunlinguabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlinguaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
